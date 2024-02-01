6b5bdd0c2e6ed895cb2cc876896ece7d96e168f9
Lucia Predescu <luciana-579@hotmail.com>
2024-02-01 10:13:26 +0100

    Add test class for employees

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,10 +1,20 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class EmployeesTest {
+
+
+
+
+
+
+
 
 
 }
