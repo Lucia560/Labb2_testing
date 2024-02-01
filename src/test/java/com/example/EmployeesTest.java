e940d3352f3836dfdb891e4c673be330920c1879
Lucia Predescu <luciana-579@hotmail.com>
2024-02-01 12:03:54 +0100

    Tests are complete for employees class

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,17 +1,66 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class EmployeesTest {
 
+ @Mock
+ private BankService bankService;
+ @Mock
+ private EmployeeRepository employeeRepository;
+ @InjectMocks
+ private Employees employees;
+ private Employee employee1;
+ private Employee employee2;
+ private Employee employee3;
+
+ @BeforeEach
+ void setUp(){
+  employee1 = new Employee("Mira Bella", 55000.0);
+  employee2 = new Employee("J.O Skinner",150000.0);
+  employee3 = new Employee("Tom Spencer",45000.0);
+  Mockito.when(employeeRepository.findAll()).thenReturn(List.of(employee1, employee2,employee3));
+ }
 
 
+ @Test
+ @DisplayName("Test if the payment is done")
+ void testIfThePaymentIsDone(){
+     int payments = employees.payEmployees();
 
+     assertEquals(3,payments);
+     Mockito.verify(bankService).pay(employee1.getId(), employee1.getSalary());
+     Mockito.verify(bankService).pay(employee2.getId(), employee2.getSalary());
+     Mockito.verify(bankService).pay(employee3.getId(),employee3.getSalary());
+     assertTrue(employee1.isPaid());
+     assertTrue(employee2.isPaid());
+     assertTrue(employee3.isPaid());
+ }
+
+ @Test
+ @DisplayName("Catch RunTimeexception")
+ void catchRunTimeException(){
+     Mockito.doThrow(RuntimeException.class).when(bankService).pay(employee1.getId(), employee1.getSalary());
+     int payments = employees.payEmployees();
+     assertEquals(2,payments);
+
+     Mockito.verify(bankService).pay(employee1.getId(), employee1.getSalary());
+     Mockito.verify(bankService).pay(employee2.getId(), employee2.getSalary());
+     assertFalse(employee1.isPaid());
+     assertTrue(employee2.isPaid());
+ }
 
 
 
